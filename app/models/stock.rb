f5faed0327b1eddec15314cf2e4315bc01d11db0
Rails Udemy Course <alicejchiu@gmail.com>
2019-07-28 02:47:15 +0000

    Added ability to search for ticker symbol and display price, company, and symbol

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,9 @@
-class Stock < ApplicationRecord
+class Stock < ActiveRecord::Base
+    
+    def self.new_from_lookup(ticker_symbol)
+      StockQuote::Stock.new(api_key: '') #add API key
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      #price = strip_commas(looked_up_stock.latest_price)
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price )
+    end
 end
